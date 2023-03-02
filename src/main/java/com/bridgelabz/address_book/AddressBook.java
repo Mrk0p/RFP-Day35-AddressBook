691fafeb4b87a5c6571349f7cfe44f01d21fc04c
Mrk0p <sachinwele23@gmail.com>
2023-03-02 17:02:35 +0530

    Added:add new contact in address book

---
+++ src/main/java/com/bridgelabz/address_book/AddressBook.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.address_book;
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+
+        Contact person1 = new Contact();
+        System.out.println("Enter details : ");
+        person1.getPersonDetails();
+    }
+}
